82410b3bb95aa1b73266f1e737d1235834038aa0
Kowantify <35680697+Kowantify@users.noreply.github.com>
2026-02-01 15:57:54 -0600

    Add files via upload

--- router/router.go
+++ router/router.go
@@ -101,6 +101,7 @@ func Configure(m *wserver.Manager, client remote.Client) *gin.Engine {
 			server.DELETE("/backups/restic/repo", restic.DeleteServerResticRepo)
 			server.POST("/backups/restic/repo/delete", restic.DeleteServerResticRepo)
 			server.GET("/backups/restic/repo/exists", restic.CheckServerResticRepo)
+			server.GET("/backups/restic/repo/size", restic.GetServerResticRepoDiskUsage)
 			server.POST("/backups/restic/:backupId/prepare", restic.PrepareServerResticBackupHandler)
 			server.GET("/backups/restic/:backupId/prepare/status", restic.GetServerResticBackupPrepareStatus)
 			server.POST("/backups/restic/:backupId/restore", restic.RestoreServerResticBackupHandler)
